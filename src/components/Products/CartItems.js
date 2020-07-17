e4b56aba115170d738cec4d37fd72d6ba86a221f
Nuanjan <Maine09102016>
2020-07-16 22:15:13 -0400

    add Cart components
    add a cart component as a parent of Item so Heding never change even
    the item delete
    
    add item coponent that
      -has img picture 100*100px
      -has item name
      -has quantities
      -has increase/decrease Button
      -has remove button

--- src/components/Products/CartItems.js
+++ src/components/Products/CartItems.js
@@ -1,54 +1,13 @@
-import React, { useState, useEffect } from 'react'
-import Item from './Item'
-import { productsIndex } from '../../api/Inventory'
-const CartItems = prop => {
-  const [cart, setCart] = useState([])
-  useEffect(() => {
-    productsIndex()
-      .then(res => setCart(res.data.products))
-      .catch(() => console.log('failed to get places'))
-  }, [])
+import React from 'react'
+import './../../index.scss'
 
-  // basic styling for Index values
-  const containerStyle = {
-    display: 'flex',
-    flexWrap: 'Wrap',
-    padding: '30px',
-    border: '1px solid black'
-  }
-  // if not item on cart say cart empty else return items on cart
-  // if (res.data.products.length === 0) {
-  //     return (
-  //     <p>Cart empty!</p>
-  //   )}else {
-  //     {cart.map(product => (
-  //       <Item
-  //         key={product.name}
-  //         name={product.name}
-  //         price={product.price}
-  //         description={product.description}
-  //         imageUrl={product.imageUrl}
-  //         category={product.category}
-  //         quantity={product.quantity}
-  //       />
-  //     ))}
-  return (
-    <div>
-      <h2>All Products</h2>
-      <div style = {containerStyle} >
-        {cart.map(product => (
-          <Item
-            key={product.name}
-            name={product.name}
-            price={product.price}
-            description={product.description}
-            imageUrl={product.imageUrl}
-            category={product.category}
-            quantity={product.quantity}
-          />
-        ))}
-      </div>
+const CartItems = props => (
+  <div>
+    <h1 className='cartName'>Cart</h1>
+    <div className="cart-box">
+      {props.children}
     </div>
-  )
-}
+  </div>
+)
+
 export default CartItems
