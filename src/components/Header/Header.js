146569951e252f1b9a9057ec94998dc4567aae62
Compenso <wesurbaniakmusic@gmail.com>
2020-07-21 07:44:11 -0600

    Header updated title
    title updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -22,7 +22,7 @@ const unauthenticatedOptions = (
 const Header = ({ user }) => (
   <Navbar bg="primary" variant="dark" expand="md">
     <Navbar.Brand href="#">
-      react-auth-template
+      Luxury-React-Auth-Template
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
