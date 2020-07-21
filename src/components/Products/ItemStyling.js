5fd82a9694c374588f52a4428e1e9879331b8639
Nuanjan <Maine09102016>
2020-07-20 21:35:47 -0400

    Edit front end code
    Edit README add link of back end and deploy
    Add styling to Cart view page

--- src/components/Products/ItemStyling.js
+++ src/components/Products/ItemStyling.js
@@ -3,6 +3,9 @@ import { destroyItemInCart } from '../../api/cartIndex'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import { Redirect } from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 const ItemStyling = ({ name, price, imageUrl, quantity, id, cartId, pushSubTotal }) => {
   const [quant, setQuantity] = useState(1)
@@ -35,36 +38,43 @@ const ItemStyling = ({ name, price, imageUrl, quantity, id, cartId, pushSubTotal
   }
 
   return (
-    <div>
-      <div className='item-box'>
-        <div className='itemImg'>
-          <img src={imageUrl} alt={name} />
-        </div>
-        <p className='amount'>Name: {name}</p>
-        <p className='amount' name='price'>Price:<span>$</span>{price}</p>
-        <Form.Label className='quan'>Quantity:</Form.Label>
-        <Form.Control
-          required
-          name="quantity"
-          value={quant}
-          type="number"
-          min="1"
-          max={quantity}
-          onChange={handleChange}
-        />
-        <Form.Text className="text-muted">
-          Price of items is: {cost}
-        </Form.Text>
-        <Button
-          className='remove-btn'
-          variant="dark"
-          type="submit"
-          onClick={destroyItem}
-        >
-      Remove
-        </Button>
-      </div>
-    </div>
+    <Container fluid='md'>
+      <Row className='row-item'>
+        <Col className='column d-flex align-items-center justify-content-center'>
+          <div className="itemImg">
+            <img src={imageUrl} alt={name} />
+          </div>
+        </Col>
+        <Col className='column-price d-flex align-items-center justify-content-center'>Name: {name}</Col>
+        <Col className='column d-flex align-items-center justify-content-center'>
+          <Form.Label className='quan'>Quantity:</Form.Label>
+          <Form.Control
+            required
+            name="quantity"
+            value={quant}
+            type="number"
+            min="1"
+            max={quantity}
+            onChange={handleChange}
+          />
+        </Col>
+        <Col className='column d-flex align-items-center justify-content-center'>
+          <Form.Text className="text-muted column-price">
+            Price of items is: <span>$</span>{cost}
+          </Form.Text>
+        </Col>
+        <Col className='column d-flex align-items-center justify-content-center'>
+          <Button
+            className='remove-btn'
+            variant="dark"
+            type="submit"
+            onClick={destroyItem}
+          >
+        Remove
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
